0a88f73d0f0e7253f9f0213ee6a310d83c052a1a
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-06-21 18:42:13 -0300

    responsividadeÇ menu hamburguer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,51 @@
 const curiosities = [
   {
-    number: '1',
+    number: "1",
     title: "Ajuda a previnir doenças cardiovasculares",
     text: "1Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
-    number: '2',
+    number: "2",
 
     title: "Ajuda nas funções cerebrais",
     text: "2Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
-    number: '3',
+    number: "3",
 
     title: " Auxilia no bom humor",
     text: "3Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
-    number: '4',
+    number: "4",
 
     title: "Melhora o sono",
     text: "4Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
-    number: '5',
+    number: "5",
 
     title: "Traz energia para o corpo e reduz cãibras",
     text: "5Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
-    number: '6',
+    number: "6",
 
     title: "É benéfica para o sistema digestivo",
     text: "6Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
 ];
 
-const content = document.querySelector(".curiosities-content");
-
 function commenterData(content) {
   const title = document.querySelector(".curiosities-title");
   const text = document.querySelector(".text");
-  const test = document.querySelector('.number')
-  
+  const test = document.querySelector(".number");
+
   let index = clickCounter % curiosities.length;
-  
+
   setTimeout(() => {
     const curiosity = curiosities[index];
-    test.textContent = curiosity.number
+    test.textContent = curiosity.number;
     title.textContent = curiosity.title;
     text.textContent = curiosity.text;
     content.classList.remove("animation-content");
@@ -69,8 +67,25 @@ function iterateArrayWithLeftButton(content) {
   commenterData(content);
 }
 
+function toggleMenu(mobileButton, event) {
+  const nav = document.querySelector(".header-nav");
+  if (mobileButton.textContent === "menu") {
+    mobileButton.textContent = "close";
+    event.currentTarget.setAttribute("aria-expanded", "true");
+    event.currentTarget.setAttribute("aria-label", "abrir menu");
+
+  } else {
+    mobileButton.textContent = "menu";
+    event.currentTarget.setAttribute("aria-expanded", "false");
+    event.currentTarget.setAttribute("aria-label", "fechar menu");
+  }
+  nav.classList.toggle("active");
+}
+
+const content = document.querySelector(".curiosities-content");
 const rightButton = document.querySelector(".right-button");
 const leftButton = document.querySelector(".left-button");
+const mobileButton = document.getElementById("btn-mobile");
 let clickCounter = 0;
 
 commenterData();
@@ -79,3 +94,4 @@ rightButton.addEventListener("click", () =>
   iterateArrayWithRightButton(content)
 );
 leftButton.addEventListener("click", () => iterateArrayWithLeftButton(content));
+mobileButton.addEventListener("click", () => toggleMenu(mobileButton, event));
