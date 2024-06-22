29eab52cf003ec335d0f5bba4430422bde7a2fff
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-06-21 21:03:09 -0300

    ref: ajsutes no código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,33 @@
 const curiosities = [
   {
     number: "1",
-    title: "Ajuda a previnir doenças cardiovasculares",
-    text: "1Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
+    title: "Ajuda a prevenir doenças cardiovasculares",
+    text: "Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
     number: "2",
-
     title: "Ajuda nas funções cerebrais",
-    text: "2Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
+    text: "Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
     number: "3",
-
-    title: " Auxilia no bom humor",
-    text: "3Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
+    title: "Auxilia no bom humor",
+    text: "Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
     number: "4",
-
     title: "Melhora o sono",
-    text: "4Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
+    text: "Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
     number: "5",
-
     title: "Traz energia para o corpo e reduz cãibras",
-    text: "5Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
+    text: "Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
   {
     number: "6",
-
     title: "É benéfica para o sistema digestivo",
-    text: "6Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
+    text: "Consumir banana regularmente ajuda nas atividades do cérebro, como raciocinar, memorizar e manter a concentração. A ingestão adequada de banana também reduz o risco de acidente vascular cerebral, conhecido como derrame. O triptofano é o responsável por isso, um aminoácido essencial para a produção de serotonina, o hormônio da felicidade.",
   },
 ];
 
@@ -48,7 +43,9 @@ function commenterData(content) {
     test.textContent = curiosity.number;
     title.textContent = curiosity.title;
     text.textContent = curiosity.text;
-    content.classList.remove("animation-content");
+    if (content && content.classList.length === 2) {
+      content.classList.remove("animation-content");
+    }
   }, 600);
 }
 
@@ -69,11 +66,11 @@ function iterateArrayWithLeftButton(content) {
 
 function toggleMenu(mobileButton, event) {
   const nav = document.querySelector(".header-nav");
+
   if (mobileButton.textContent === "menu") {
     mobileButton.textContent = "close";
     event.currentTarget.setAttribute("aria-expanded", "true");
     event.currentTarget.setAttribute("aria-label", "abrir menu");
-
   } else {
     mobileButton.textContent = "menu";
     event.currentTarget.setAttribute("aria-expanded", "false");
@@ -88,10 +85,19 @@ const leftButton = document.querySelector(".left-button");
 const mobileButton = document.getElementById("btn-mobile");
 let clickCounter = 0;
 
-commenterData();
+if (content) {
+  commenterData(content);
 
-rightButton.addEventListener("click", () =>
-  iterateArrayWithRightButton(content)
-);
-leftButton.addEventListener("click", () => iterateArrayWithLeftButton(content));
-mobileButton.addEventListener("click", () => toggleMenu(mobileButton, event));
+  rightButton.addEventListener("click", () =>
+    iterateArrayWithRightButton(content)
+  );
+  leftButton.addEventListener("click", () =>
+    iterateArrayWithLeftButton(content)
+  );
+
+  mobileButton.addEventListener("click", (event) =>
+    toggleMenu(mobileButton, event)
+  );
+} else {
+  console.error("Elemento '.curiosities-content' não encontrado.");
+}
